50885f3159cc6a7a187dcaeb33423d7563b317f5
Ionut Lazar <lionut@mailbox.org>
2022-09-20 11:13:25 +0300

    Add navbar and categories menu

---
+++ src/components/CategoriesMenu.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { Menu, MenuItem } from "@mui/material";
+import { AxiosRequestConfig } from "axios";
+
+import useFetchData from "../hooks/fetchData";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function CategoriesMenu() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const getCategoriesRequestConfig: AxiosRequestConfig = {
+    url: "/categories",
+    method: "GET",
+  };
+
+  const categories = useFetchData(getCategoriesRequestConfig);
+
+  return (
+    <div>
+      <Button
+        sx={{ my: 2, color: "white", display: "block" }}
+        onClick={handleClick}
+      >
+        Categories
+      </Button>
+      <Menu
+        id="basic-menu"
+        open={open}
+        onClose={handleClose}
+      >
+        {categories.data.map((category: any) => (
+          <MenuItem onClick={handleClose}>{category.name}</MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
